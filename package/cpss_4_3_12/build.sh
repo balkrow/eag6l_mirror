1bd29813a279378583b722e652d1abbdcd1b7f14
Hwang In Sung <balkrow@hfrnet.com>
2024-05-21 18:59:31 +0900

    feat : [#31] DB-98DX7312-8SFP1QSFP 용 setup.env 추가 및 개발환경 수정

--- package/cpss_4_3_12/build.sh
+++ package/cpss_4_3_12/build.sh
@@ -5,9 +5,16 @@ INSTALL_ROOT=${TOPDIR}/rootfs
 branch_name=`git symbolic-ref --short HEAD` 
 
 function _clean {
+
+  if [ ${BUILD_BOARD_NAME} == "eag6l" ] ; then
 	[ -f Makefile ] && make ${MAKEOPT} TARGET=armv7 FAMILY=DX clean || exit -1
 	[ -f Makefile ] && make ${MAKEOPT} TARGET=armv7 FAMILY=DX KERNEL_FOLDER=../../build/kernel clean || exit -1
 	[ -f Makefile ] && unset CROSS_COMPILE; unset ARCH; make ${MAKEOPT} TARGET=sim64 FAMILY=DX clean  || exit -1
+  elif [ ${BUILD_BOARD_NAME} == "ac5x_db" ] ; then
+	[ -f Makefile ] && make ${MAKEOPT} TARGET=armv8 FAMILY=DX clean || exit -1
+	[ -f Makefile ] && make ${MAKEOPT} TARGET=armv8 FAMILY=DX KERNEL_FOLDER=../../build/kernel clean || exit -1
+	[ -f Makefile ] && unset CROSS_COMPILE; unset ARCH; make ${MAKEOPT} TARGET=sim64 FAMILY=DX clean  || exit -1
+  fi
 }
 
 function _config {
@@ -16,8 +23,13 @@ function _config {
 
 function _build {
 	#make ${MAKEOPT} TARGET=armv7 FAMILY=DX CONFIG_PRV_CPSS_MAX_PP_DEVICES_CNS=2 CONFIG_CPSS_MAX_PORTS_NUM_CNS=128 LOG_ENABLE=n CMD_LUA_CLI=n appDemo || exit -1
+  if [ ${BUILD_BOARD_NAME} == "eag6l" ] ; then
 	make ${MAKEOPT} TARGET=armv7 FAMILY=DX CONFIG_PRV_CPSS_MAX_PP_DEVICES_CNS=2 CONFIG_CPSS_MAX_PORTS_NUM_CNS=128 appDemo || exit -1
 	make ${MAKEOPT} TARGET=armv7 FAMILY=DX KERNEL_FOLDER=../../build/kernel modules || exit -1
+  elif [ ${BUILD_BOARD_NAME} == "ac5x_db" ] ; then
+	make ${MAKEOPT} TARGET=armv8 FAMILY=DX CONFIG_PRV_CPSS_MAX_PP_DEVICES_CNS=2 CONFIG_CPSS_MAX_PORTS_NUM_CNS=128 appDemo || exit -1
+	make ${MAKEOPT} TARGET=armv8 FAMILY=DX KERNEL_FOLDER=../../build/kernel modules || exit -1
+  fi
 }
 
 function _demo {
@@ -26,9 +38,15 @@ function _demo {
 
 
 function _install {
+  if [ ${BUILD_BOARD_NAME} == "eag6l" ] ; then
 	cp -avf ./compilation_root/${branch_name}/armv7_DX/appDemo ${INSTALL_ROOT}/sbin
 	${CROSS_COMPILE}strip ${INSTALL_ROOT}/sbin/appDemo
 	cp -avf cpssEnabler/mainExtDrv/src/gtExtDrv/linuxNoKernelModule/drivers/mvcpss.ko ${INSTALL_ROOT}/lib/modules
+  elif [ ${BUILD_BOARD_NAME} == "ac5x_db" ] ; then
+	cp -avf ./compilation_root/${branch_name}/armv8_DX/appDemo ${INSTALL_ROOT}/sbin
+	${CROSS_COMPILE}strip ${INSTALL_ROOT}/sbin/appDemo
+	cp -avf cpssEnabler/mainExtDrv/src/gtExtDrv/linuxNoKernelModule/drivers/mvcpss.ko ${INSTALL_ROOT}/lib/modules
+  fi
 	exit 0
 }
 
